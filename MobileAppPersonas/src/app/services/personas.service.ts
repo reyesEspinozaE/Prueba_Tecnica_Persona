4f95d6ffdc5a73a4603a86e67914b64d6630e4cf
Everth Reyes E. <everthreyes13@gmail.com>
2025-06-25 18:41:36 -0600

    Creación de páginas

--- MobileAppPersonas/src/app/services/personas.service.ts
+++ MobileAppPersonas/src/app/services/personas.service.ts
@@ -1,5 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+
+export interface Persona {
+  nombre: string;
+  apellido: string;
+  fechaNacimiento: string;
+  email: string;
+  telefono?: string;
+  direccion?: string;
+}
+
+type ApiResponse = { page: number, per_page: number, total: number, total_pages: number, results: Persona[]}
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +20,9 @@ export class PersonasService {
 
   httpClient = inject(HttpClient);
   
-  constructor() { }
+  getAll() {
+    return firstValueFrom(
+      this.httpClient.get('')
+    )
+  }
 }
--- MobileAppPersonas/src/main.ts
+++ MobileAppPersonas/src/main.ts
@@ -1,16 +1,22 @@
+import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment.prod';
 import { provideHttpClient } from '@angular/common/http';
+
+if (environment.production) {
+  enableProdMode();
+}
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideRouter(routes),
     provideHttpClient()
   ],
 });
