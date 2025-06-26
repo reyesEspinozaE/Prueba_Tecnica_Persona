094e20ee36cc22e18c147da6e2547152ed2db13a
Everth Reyes E. <everthreyes13@gmail.com>
2025-06-26 05:22:57 -0600

    Fix de las variables de entorno en interceptors

--- MobileAppPersonas/src/app/interceptors/auth.interceptor.ts
+++ MobileAppPersonas/src/app/interceptors/auth.interceptor.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  private readonly token = 'KvoVsjSoPGH9ojSB3x3QE4BVWl4m6unW6VTwpPoXZI';
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    if (request.url.includes('localhost:7163')) {
+    if (request.url.includes(environment.apiUrl) || request.url.startsWith(environment.apiUrl)) {
       const authenticatedRequest = request.clone({
         setHeaders: {
-          'Authorization': `Bearer ${this.token}`,
+          'Authorization': `Bearer ${environment.apiToken}`,
           'Content-Type': 'application/json',
           'Accept': 'application/json'
         }
